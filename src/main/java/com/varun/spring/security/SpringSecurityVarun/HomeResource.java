261ae161cc59318ba5eebd74be8d1e28cf7b6a14
Varun Jaiswal <varunjaiswal1891>
2023-05-01 11:39:44 +0530

    test commit

--- src/main/java/com/varun/spring/security/SpringSecurityVarun/HomeResource.java
+++ src/main/java/com/varun/spring/security/SpringSecurityVarun/HomeResource.java
@@ -9,6 +9,6 @@ public class HomeResource {
     @GetMapping("/")
     public String home()
     {
-        return ("<h1>Welcome varun jaiswal</h1>");
+        return ("<h1>Welcome varun jaiswal - first page</h1>");
     }
 }
