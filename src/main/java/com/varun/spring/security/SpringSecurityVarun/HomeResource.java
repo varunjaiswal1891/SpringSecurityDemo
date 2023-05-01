9504069756862e7231137dbe079fc07d72a82c08
Varun Jaiswal <varunpandey511@gmail.com>
2023-05-01 18:23:16 +0530

    added authorization changes

--- src/main/java/com/varun/spring/security/SpringSecurityVarun/HomeResource.java
+++ src/main/java/com/varun/spring/security/SpringSecurityVarun/HomeResource.java
@@ -9,6 +9,18 @@ public class HomeResource {
     @GetMapping("/")
     public String home()
     {
-        return ("<h1>Welcome varun jaiswal  page added</h1>");
+        return ("<h1>Welcome</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin()
+    {
+        return ("<h1>Welcome admin</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user()
+    {
+        return ("<h1>Welcome user</h1>");
     }
 }
--- src/main/java/com/varun/spring/security/SpringSecurityVarun/SecurityConfiguration.java
+++ src/main/java/com/varun/spring/security/SpringSecurityVarun/SecurityConfiguration.java
@@ -2,11 +2,13 @@ package com.varun.spring.security.SpringSecurityVarun;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -15,11 +17,16 @@ public class SecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager userDetailsService()
     {
-        UserDetails user  = User.withUsername("blah")
+        UserDetails user1  = User.withUsername("blah")
                                 .password("blah")
-                                .roles("USER")
+                                .roles("ADMIN")
                                 .build();
-        return new InMemoryUserDetailsManager(user);
+
+        UserDetails user2  = User.withUsername("foo")
+                                .password("foo")
+                                .roles("USER")
+                                .build();                       
+        return new InMemoryUserDetailsManager(user1,user2);
     }
 
     
@@ -28,6 +35,21 @@ public class SecurityConfiguration {
     {
         return NoOpPasswordEncoder.getInstance();
     }
+
+    //working of authorization using HttpSecurity
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers("/admin").hasRole("ADMIN")
+        .antMatchers("/user").hasAnyRole("USER","ADMIN")
+        .antMatchers("/").permitAll()
+        .and().formLogin();
+
+        return http.build();
+     }
     
     
 }
